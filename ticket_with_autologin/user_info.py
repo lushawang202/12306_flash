2c646f81c782e5cdcebff30b18ca5437d42b94b0
lushawang202 <lucia111202@126.com>
2020-07-26 22:06:19 +0800

    add account/password can't be none support

--- ticket_with_autologin/user_info.py
+++ ticket_with_autologin/user_info.py
@@ -4,3 +4,16 @@ import getpass
 
 username = input("请输入12306账号：")
 password = getpass.getpass("请输入密码：")
+
+while True:
+    username = input("请输入12306账号：")
+    if username != '':
+        break
+    else:
+        print("用户名不能为空")
+while True:
+    password = getpass.getpass("请输入密码：")
+    if password != '':
+        break
+    else:
+        print('密码不能为空')
