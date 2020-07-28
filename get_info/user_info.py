c799de8b410b29c65408de2aac77b3ec4565f56c
lushawang202 <lucia111202@126.com>
2020-07-28 12:10:59 +0800

    add reading account record support

--- get_info/user_info.py
+++ get_info/user_info.py
@@ -1,16 +1,30 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 import getpass
+import shelve
 
 while True:
-    username = input("请输入12306账号：")
-    if username != '':
-        break
-    else:
-        print("用户名不能为空")
-while True:
-    password = getpass.getpass("请输入密码：")
-    if password != '':
-        break
-    else:
-        print('密码不能为空')
+    with shelve.open('./info') as db:
+        account_repeat = input(f'是否使用账号{db["username"]}登录？回车默认，按1重新录入')
+        if account_repeat == '':
+            username = db['username']
+            password = db['password']
+            break
+        if account_repeat == '1':
+            while True:
+                username = input("请输入12306账号：")
+                if username != '':
+                    db['username'] = username
+                    break
+                else:
+                    print("用户名不能为空")
+            while True:
+                password = getpass.getpass("请输入密码：")
+                if password != '':
+                    db['password'] = password
+                    break
+                else:
+                    print('密码不能为空')
+            break
+        else:
+            print("你很淘气哟")
--- ticket_with_autologin/12306_flash+.py
+++ ticket_with_autologin/12306_flash+.py
@@ -9,8 +9,8 @@ from ticket_with_autologin.auto_login import Auto_Login
 if __name__ == '__main__':
     print('😊全自动抢票小助手为您服务！请填写信息开始抢票（直接回车表示默认）。')
     from get_info.train_repeat import *
-    from get_info.browser_info import *
     from get_info.user_info import *
+    from get_info.browser_info import *
     print('请稍后，即将开始抢票！')
     auto_login = Auto_Login(driver, username, password)
     auto_login.auto_login().check_ticket(start, end, date, time_period).buy_ticket(train).pay(who, seat)
