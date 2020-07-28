acbec5488a8a4fa11e506feffc48fa0430f87b3e
lushawang202 <lucia111202@126.com>
2020-07-28 17:33:42 +0800

    add query available trains before check trains

--- page/ticket.py
+++ page/ticket.py
@@ -2,19 +2,15 @@
 # -*- coding: utf-8 -*-
 
 import winsound
-
-from selenium.common.exceptions import NoSuchElementException
 from selenium.webdriver.common.by import By
-from selenium.webdriver.support import expected_conditions
 from selenium.webdriver.support.select import Select
-from selenium.webdriver.support.wait import WebDriverWait
-
 from page.base import Base
 from page.pay import Pay
 
 
 class Ticket(Base):
     _url = 'https://kyfw.12306.cn/otn/leftTicket/init?linktypeid=dc'
+    available_trains = []
 
     def check_ticket(self, start, end, date, time_period):
         # 设置from_city to_city date time
@@ -27,47 +23,56 @@ class Ticket(Base):
         to_city = self.find(By.ID, 'toStationText')
         to_city.click()
         to_city.send_keys(f'{end}\n')
-        # action_chains = ActionChains(self._driver)
-        # action_chains.send_keys(Keys.ARROW_DOWN).send_keys(Keys.ARROW_DOWN).send_keys(Keys.ENTER).perform()
-        self._driver.execute_script(
+        # action_chains().send_keys(Keys.ARROW_DOWN).send_keys(Keys.ARROW_DOWN).send_keys(Keys.ENTER).perform()
+        self.execute_script(
             f'a=document.getElementById("train_date");a.removeAttribute("readonly");a.value="{date}"')
         time_period_ele = self.find(By.ID, 'cc_start_time')
         Select(time_period_ele).select_by_visible_text(f'{time_period}')
         self.find(By.ID, 'query_ticket').click()
         return self
 
-    def buy_ticket(self, trains):
+    # 获得可用车次
+    def acquire_available_train(self, trains):
         trains = [x for x in trains.split(' ')]
+        self.implicitly_wait(0.5)
         while True:
-            WebDriverWait(self._driver, 3).until(expected_conditions.element_to_be_clickable((By.ID, 'wf')))
-            count = 0
-            flag = 1
-            for train in trains:
-                self._driver.implicitly_wait(0.5)
-                if self.finds(By.XPATH, f'//a[text()="{train}"]'):
-                    flag = 0
+            self.wait_to_click(3, (By.ID, 'wf'))
+            if self.finds(By.ID, 'float'):
+                for train in trains:
+                    if self.finds(By.XPATH, f'//*[text()="{train}"]'):
+                        self.available_trains.append(train)
+                        print(self.available_trains)
+                self.implicitly_wait(8)
+                break
+            elif self.find(By.XPATH, '//*[contains(text(),"稍后再试")]').is_displayed():
+                self.refresh()
+            else:
+                self.screen_shot('./获取车次异常.png')
+                print('获取车次异常')
+                self.refresh()
+
+    def buy_ticket(self, trains):
+        self.acquire_available_train(trains)
+        if len(self.available_trains) == 0:
+            print('\n我不得不退出，因为当前搜索没有你说的车。')
+            raise Exception
+        else:
+            while True:
+                for train in self.available_trains:
                     if self.find(By.XPATH, f'//a[text()="{train}"]/../../../../../td[4]').text != '候补':
                         self.find(By.XPATH, f'//a[text()="{train}"]/../../../../../td[last()]').click()
-                        try:
-                            self._driver.implicitly_wait(8)
-                            self.find(By.ID, 'submitOrder_id')
-                            winsound.Beep(600, 1000)
-                            print("恭喜小主抢到啦！快去付款吧~")
-                            return Pay(self._driver)
-                        except NoSuchElementException:
+                        self.implicitly_wait(0.3)
+                        if self.finds(By.XPATH, '//*[contains(text(), "当前时间不可以订票")]'):
                             print('抢票失败：当前时间不可以订票')
                             raise Exception
+                        elif self.finds(By.XPATH, '//div[contains(text(),"您选择的列车距开车时间很近了")]'):
+                            self.find(By.ID, 'qd_closeDefaultWarningWindowDialog_id').click()
+                        else:
+                            if self.find(By.ID, 'submitOrder_id').is_displayed():
+                                winsound.Beep(600, 1000)
+                                print("恭喜小主抢到啦！快去付款吧~")
+                                return Pay(self._driver)
+                            else:
+                                self.screen_shot('./订票跳转失败.png')
+                self.find(By.ID, 'query_ticket').click()
 
-                elif flag:
-                    if self.find(By.XPATH, '//*[contains(text(),"稍后再试")]').is_displayed():
-                        break
-                    else:
-                        count += 1
-                        if count == len(trains):
-                            print('\n我不想再继续了，因为当前搜索没有你说的车。')
-                            raise Exception
-
-            self.find(By.ID, 'query_ticket').click()
-
-    def quit(self):
-        self._driver.quit()
