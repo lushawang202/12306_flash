a7a709156ad1a1b7415f7c4058d4f956de466da4
lushawang202 <lucia111202@126.com>
2020-07-28 17:36:39 +0800

    delete non-used rows

--- ticket_with_autologin/12306_flash+.py
+++ ticket_with_autologin/12306_flash+.py
@@ -1,13 +1,11 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-
 # 自动登录，查票，买票，加浏览器选择
-
 from ticket_with_autologin.auto_login import Auto_Login
 
 if __name__ == '__main__':
-    print('😊全自动抢票小助手为您服务！请填写信息开始抢票（直接回车表示默认）。')
+    print('O(∩_∩)O全自动抢票小助手为您服务！请填写信息开始抢票（直接回车表示默认）。')
     from get_info.train_repeat import *
     from get_info.user_info import *
     from get_info.browser_info import *
