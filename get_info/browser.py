c82e27b9e58620b48db75a9770cfeb3befb72b4e
lushawang202 <lucia111202@126.com>
2020-07-30 14:52:34 +0800

    Update to PO mode for all func modules.

---
+++ get_info/browser.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+import shelve
+
+
+class Browser:
+    def __init__(self):
+        with shelve.open('./info') as db:
+            self.driver_mark = input(f"请输入要使用的浏览器（默认为{db['self._driver']}）\n1：firefox\n2：chrome\n")
+            self.driver = db['self._driver']
+
+    def choose_browser(self):
+        while True:
+            with shelve.open('./info') as db:
+                if self.driver_mark == '1':
+                    self.driver = 'firefox'
+                    db['self._driver'] = 'firefox'
+                    return self.driver
+                elif self.driver_mark == '2':
+                    self.driver = 'chrome'
+                    db['self._driver'] = 'chrome'
+                    return self.driver
+                elif self.driver_mark == '':
+                    return self.driver
+                else:
+                    print('咋个意思？')
